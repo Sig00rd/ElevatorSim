fffac78a41e945337fc710cbfa5949cba8c71d07
Dawid Godek <dawid.gdk@gmail.com>
2019-01-22 06:23:49 +0100

    Minor update

--- src/app.erl
+++ src/app.erl
@@ -3,7 +3,7 @@
 -include("config.hrl").
 
 %% API
--export([start/0]).
+-export([start/0, input_redirect/2, simulation/1]).
 
 start() ->
   Drawer = spawn(drawer, drawer, []),
@@ -12,9 +12,14 @@ start() ->
   Control_system = spawn(control_system, control_system, [Elevator, Floors]),
   utils:broadcast({set_control_system, Control_system}, [Elevator|Floors]),
   Dude_generator = spawn(dude_generator, dude_generator, [Floors]),
-  Input = spawn(input, startInput, [self()]),
+  Input_redirect = spawn(app, input_redirect, [self(), Dude_generator]),
+  Input = spawn(input, startInput, [Input_redirect]),
   PIDs_that_accept_step = [Dude_generator, Control_system],
-  simulation(PIDs_that_accept_step).
+  _ = spawn(app, simulation, [PIDs_that_accept_step]),
+  receive
+    exit ->
+      io:format("Gonna die :( \n")
+  end.
 
 
 simulation(PIDs) ->
@@ -30,3 +35,12 @@ spawn_floors(Floors_list, 0, _) ->
 spawn_floors(Floors_list, Number_of_floors, Drawer) ->
   New_floor = spawn(floor, floor, [Number_of_floors, Drawer]),
   spawn_floors([New_floor|Floors_list], Number_of_floors-1, Drawer).
+
+input_redirect(Main, Dude_generator) ->
+  receive
+    exit ->
+      Main ! exit;
+    {newDude, From, To} ->
+      Dude_generator ! {newDude, From, To},
+      input_redirect(Main, Dude_generator)
+  end.
